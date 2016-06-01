2744051e46866edc898b37726e5f0ba6ebe95b65
hd016 <hd016@hdm-stuttgart.de>
2016-06-02 01:02:17 +0200

    Update parser.java

--- parser.java
+++ parser.java
@@ -1,42 +1,35 @@
+
 package textmining_bibel;
-import java.io.BufferedReader; 
-import java.io.File; 
-import java.io.FileReader; 
-import java.io.IOException; 
 
-public class parser {
-
-
-	    private static void ladeDatei(String datName) { 
-
-	        File file = new File(datName); 
-
-	        if (!file.canRead() || !file.isFile()) 
-	            System.exit(0); 
-
-	            BufferedReader in = null; 
-	        try { 
-	            in = new BufferedReader(new FileReader(datName)); 
-	            String zeile = null; 
-	            while ((zeile = in.readLine()) != null) { 
-	                System.out.println(zeile); 
-	            } 
-	        } catch (IOException e) { 
-	            e.printStackTrace(); 
-	        } finally { 
-	            if (in != null) 
-	                try { 
-	                    in.close(); 
-	                } catch (IOException e) { 
-	                } 
-	        } 
-	    } 
-
-	    public static void main(String[] args) { 
-	        String dateiName = "Martin_Luther_Uebersetzung_1912.txt"; 
-	        ladeDatei(dateiName); 
-	    } 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+ 
+public class bparser
+{
+	public static void main(String[] args) throws FileNotFoundException, IOException
+	{
+		String filePath = "iso88";
+		File file = new File(filePath);
+		if(file.isFile())
+		{
+			FileInputStream inputStream = new FileInputStream(file);
+			InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("ISO-8859-15"));			
+			BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+			StringBuilder stringBuilder = new StringBuilder();
+ 
+			stringBuilder.append(bufferedReader.readLine());
+			while(bufferedReader.ready())
+			{
+				stringBuilder.append("\r\n");
+				stringBuilder.append(bufferedReader.readLine());				
+			}
+ 
+			System.out.println(stringBuilder.toString());
+		}				
 	}
-
-
-
+}
