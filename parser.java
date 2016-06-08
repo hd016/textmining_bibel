7f59e2a33639447d93f2d86ee5accac82270e6da
hd016 <hd016@hdm-stuttgart.de>
2016-06-08 15:25:07 +0200

    update hashmap

--- parser.java
+++ parser.java
@@ -31,106 +31,46 @@ import java.util.regex.Pattern;
  
 public class bparser
 {
-	public static void main(String[] args) throws 
-    IOException {
-		
-		navi();
-}
-
-	public static int navi() throws FileNotFoundException, IOException{
-		
-		System.out.println("#NAVI#\n 1 - Parsen \n 2 - Wortsuche \n Bitte geben Sie eine Zahl an:");
-		int auswahl;
-		Scanner input = new Scanner(System.in);
-		auswahl = input.nextInt();
-		System.out.println();
-		auswahl_in(auswahl);
-		return auswahl;
-		
-	}
-	
-	private static int auswahl_in(int auswahl) throws FileNotFoundException, IOException {
-		
-		int auswahl_in;
-		auswahl_in = auswahl;
-		if(auswahl_in == 1)
-		{
-			parsen();
-		}
-		else if(auswahl_in == 2)
-		{
-			//gorevli();
-			
-		}
-		else if(auswahl_in == 3)
-		{
-			System.out.println("null");
-		}
-		else 
-		{
-			System.out.println("null");
-			auswahl_in(auswahl);
-		}
-		return auswahl_in;
-	}
-	
-	public static String parsen() throws IOException, FileNotFoundException {
-		
-		String filePath = "iso88";
-		File file = new File(filePath);
-		FileInputStream inputStream = new FileInputStream(file);	
-		
-		InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("ISO-8859-15"));
-		BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-		StringBuilder stringBuilder = new StringBuilder();
-		String s = "";
-		
-		Pattern p = Pattern.compile("([A-Z][a-z]{2,2}|[1-9][A-Z][a-z]) [1-9][0-9]*:[1-9][0-9]*");
-		
-		if (file.exists()) {
-			
-		  while (bufferedReader.ready()) {
-          s = bufferedReader.readLine();
-          Matcher m = p.matcher(s);
-          
-          stringBuilder.append(m.replaceAll("") + "\n");
+	public static HashMap<String, Integer> wordMap = new HashMap<>();
+	 
+    public static void main(String[] args) throws FileNotFoundException,
+                IOException {
+          parse();
     }
-		    //System.out.println(stringBuilder.toString());
-		  
-		  wortersuche(stringBuilder.toString());
-				
-		
-	}
-		return stringBuilder.toString();
-	}
-	
-	public static String wortersuche(String text){
-		
-		HashMap<String, Integer> hash = new HashMap<>();
 
-    	// text = text.replaceAll("[^a-zA-Z 0-9]", ""); //OHNE UMLAUTE
-    	// "[_[^\\w\\däüöÄÜÖ\\+\\- ]]", ""				// MIT UMLAUTE
-		
-    	text = text.replaceAll("[_[^\\w\\däüöÄÜÖ\\+\\- ]]", "");
+    public static void parse() throws IOException, FileNotFoundException {
 
-    	// ^/(?!ignoreme|ignoreme2|ignoremeN) 
-	    String [] tokens = text.split("[\\s']");
-		Integer value = 0;
+          String filePath = "iso88";
+          File file = new File(filePath);
+          FileInputStream inputStream = new FileInputStream(file);
 
-        for(String s:tokens){
-        	if(hash.containsKey(s)){
-        		s.indexOf(value);
-        	}
-        	else{
-            	hash.put(s,value);
+          InputStreamReader inputStreamReader = new InputStreamReader(
+                      inputStream, Charset.forName("ISO-8859-15"));
+          BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+          StringBuilder stringBuilder = new StringBuilder();
+          String s = "";
 
-        	}
-	}
-		System.out.println(hash.size());
-		System.out.println(hash);
-        
-		return text;
-		}
-	
-	
-	}
+          Pattern p = Pattern
+                      .compile("([A-Z][a-z]{2,2}|[1-9][A-Z][a-z]) [1-9][0-9]*:[1-9][0-9]*");
+
+          if (file.exists()) {
+
+                while (bufferedReader.ready()) {
+                      s = bufferedReader.readLine();
+                      Matcher m = p.matcher(s);
+
+                      stringBuilder.append(m.replaceAll("") + "\n");
+                }
+          }
+          String text[] = stringBuilder.toString()
+                      .replaceAll("[_[^\\w\\däüöÄÜÖ\\+\\- ]]", "").split(" ");
+          for (int i = 0; i < text.length; i++) {
+                if (wordMap.containsKey(text[i]))
+                      wordMap.put(text[i], wordMap.get(text[i]) + 1);
+                else
+                      wordMap.put(text[i], 1);
+          }
+          System.out.println(wordMap.toString());
+    }
+
+}
